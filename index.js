2b86344497f861d37b9563138340ae9e7fc1144c
Gabriel Soares <gabrielsoacc@gmail.com>
2025-08-04 23:37:43 -0300

    feat: added prompt for response only about real travel places

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import { GoogleGenAI } from "@google/genai";
+import { doQuestion } from "./questions.js";
 
 const ai = new GoogleGenAI({});
 
 async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: "Descreva pra mim, todos os principais pokemons da história, sem muitos detalhes, mas por ranking"
-  });
-  console.log(response.text);
+
+    let prompt = "Você é um site de viagens, fale apenas sobre locais que realmente existem na terra." +
+    " Se o usuário digitar qualquer coisa relacionado a outro assunto ou um lugar que não existe de verdade, informe que" +
+    " você não de falar sobre isso. Antes de dar a responsta negativa (se pode ou não falar sobre isso), pesquise se existe realmente um lugar com o nome solicitado," 
+    + " mas não fale sobre isso, apenas dê informações sobre o local, como um guia turístico."
+    + "Mensagem do usuário: "; 
+
+    prompt += await doQuestion("Me fale sobre o destino que deseja conhecer: ");
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: prompt
+    });
+    console.log(response.text);
 }
 
 main();
